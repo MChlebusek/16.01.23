1e8feac12eaaa8d5e64185e83c39c173e2b34a76
opilane <opilane@HP-CZC2349HTZ>
2023-01-16 16:47:56 +0200

    Add project files.

---
+++ MyModule.py
@@ -0,0 +1,13 @@
+from math import *
+from random import choice
+
+import string
+
+def salasona(k: int):
+    sala=""
+    for i in range(k):
+        t=choice(string.ascii_letters)
+        num=choice([1,2,3,4,5,6,7,8,9,0])
+        t_num=[t,str(num)]
+        sala+=choice(t_num)
+        return sala
---
+++ _16.01.23.py
@@ -0,0 +1,39 @@
+﻿from operator import xor
+from MyModule import *
+flag=True
+while flag:
+    a=int(input("Registreerimine-1, autoriseerimine-2 exit-3: "))
+    if a==1:
+        login=input("login: ")
+        b=input("automaatne parooli genereerimine-1,  sõltumatu-2 ")
+        if b==1: 
+            sala=salasona(10)
+            print(sala)
+        else:
+            password = input("password:")
+            password2 = input("password:")
+            while len(password) < 10:
+                print("Короткий!")
+                password = input("password:")
+                password2 = input("password:")
+            while "123" in password:
+                print("Простой!")
+                password = input("password:")
+                password2 = input("password:")
+            while password2 != password:
+                print("Различаются.")
+                password = input("password:")
+                password2 = input("password:")
+        print ("olete registreerunud")
+    elif a==2:
+        login=input("login: ")
+        password = input("password:")
+        print("olete edukalt sisse loginud")
+    elif a==3:
+        print("vajutage klaviatuuril (esc)")
+    else:
+        print("error")
+
+
+   
+
